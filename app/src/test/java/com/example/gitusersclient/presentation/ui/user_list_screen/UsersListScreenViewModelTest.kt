90dc34180f85b7d3a57e7b54970f882ad0ff80eb
😎👉Philippoid <temp-pylyp.buhaievskyi@cafu.com>
2024-03-16 19:12:48 +0100

    Test: code clean up

--- app/src/test/java/com/example/gitusersclient/presentation/ui/user_list_screen/UsersListScreenViewModelTest.kt
+++ app/src/test/java/com/example/gitusersclient/presentation/ui/user_list_screen/UsersListScreenViewModelTest.kt
@@ -3,7 +3,6 @@ package com.example.gitusersclient.presentation.ui.user_list_screen
 import com.example.core.abstraction.data.IRepository
 import com.example.core.abstraction.domain.IBusinessModel
 import com.example.core.common.DataResult
-import com.example.gitusersclient.domain.model.UserBM
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
